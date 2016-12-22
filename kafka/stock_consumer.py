70ce86cebff820cedd8b31786a27d5f684b29730
kevin <xjiahang@hotmail.com>
2016-12-21 23:06:02 -0800

    Add argparse module to parse command line input

--- kafka/stock_consumer.py
+++ kafka/stock_consumer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python 
 
 from kafka import KafkaConsumer
+import argparse
 
 if __name__ == "__main__":
-    consumer = KafkaConsumer("stock");
+    parser = argparse.ArgumentParser()
+    parser.add_argument('kafka_url', help = 'kafka broker url')
+    parser.add_argument('topic', help = 'topic name that consumer subscribes')
+    args = parser.parse_args()
+
+    consumer = KafkaConsumer(args.topic, bootstrap_servers = [args.kafka_url])
     for message in consumer:
         print("topic : %s, message value %s" % (message.topic, message.value))
--- kafka/stock_producer.py
+++ kafka/stock_producer.py
@@ -5,22 +5,29 @@ from kafka import KafkaProducer
 import json
 import schedule 
 import time
+import argparse
 
 # get stock data structure stock_json from third party module - googlefinance 
-def send_stock_info(producer, symbol):
+def send_stock_info(producer, topic, symbol):
     stock_json = getQuotes(symbol)
     stock_symbol = stock_json[0]['StockSymbol']
     stock_lastTradeTime = stock_json[0]['LastTradeTime']
     stock_lastTradePrice = float(stock_json[0]['LastTradePrice'])
     print(stock_symbol, stock_lastTradeTime, stock_lastTradePrice)
     payload = ('{"StockSymbol : %s, "LastTradeTime" : %s, "LastTradePrice" : %.2f}' % (stock_symbol, stock_lastTradeTime, stock_lastTradePrice)).encode("utf-8")
-    producer.send("stock", payload)
+    producer.send(topic, payload)
 
 if __name__ == "__main__":
-    print("haha1")
-    producer = KafkaProducer()
-    schedule.every().second.do(send_stock_info, producer, 'FB')
-    print("haha2")
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('kafka_url', help = 'url of kafka broker')
+    parser.add_argument('topic', help = 'kakfa topic that the message published to')
+    parser.add_argument('stock_symbol', help = 'stock symbol users want to view')
+    args = parser.parse_args()
+
+
+    producer = KafkaProducer(bootstrap_servers = [args.kafka_url])
+    schedule.every().second.do(send_stock_info, producer, args.topic, args.stock_symbol)
     while True:
         schedule.run_pending()
         time.sleep(4)
